9b3c2195f9bf9081d9291eb66dbcd53a9de6b7b4
jemendez10 <jemendez10@misena.edu.co>
2019-06-21 09:37:35 -0500

    nuevo cambio

--- javaarreglos.js
+++ javaarreglos.js
@@ -19,6 +19,21 @@ numchasis:2456
 
 },
 {
+marca:"BMW",
+modelo:"r8",
+referencia:2020,
+numchasis:2345
+
+
+},
+{
+marca:"Audi",
+modelo:"x6",
+referencia:2019,
+numchasis:2456
+
+},
+{
 marca:"Chevrolet",
 modelo:"Captiva",
 referencia:2017,
@@ -26,15 +41,32 @@ numchasis:2882
 }
 ]
 
- for (var i = 0; i < arreglocarros.length; i++) {
-	// alert(arreglocarros[i].marca+" "+arreglocarros[i].modelo)
-	if (arreglocarros[i].referencia < 2019) {
-		alert(arreglocarros[i].marca + " su tecnomecanica tiene un valor de 100000")
+//  for (var i = 0; i < arreglocarros.length; i++) {
+// 	// alert(arreglocarros[i].marca+" "+arreglocarros[i].modelo)
+// 	if (arreglocarros[i].referencia < 2019) {
+// 		alert(arreglocarros[i].marca + " su tecnomecanica tiene un valor de 100000")
 	
-	}
-else{
-	alert(arreglocarros[i].marca + "su tecnomecanica tiene un valor de 900000")
+// 	}
+// else{
+// 	alert(arreglocarros[i].marca + "su tecnomecanica tiene un valor de 900000")
+// }
+		
+// }
+// while(arreglocarros[0].referencia > 2009){
+// 	console.log (arreglocarros[0])
+// arreglocarros[0].referencia= arreglocarros[0].referencia-1
+// }
+var marcacarro=prompt("ingrese una marca de carro")	
+switch (marcacarro){
+	case "BMW":
+	alert("Estos son los carros BMW")
+break;
+	case "Chevrolet":
+	alert("Estos son los carros Chevrolet")
+break;
+	case "Audi":
+	alert("estos son los carros Audi")
+	break;
+	default:
+	alert("ingrese nombre de algun carro")
 }
-			
-}
-
